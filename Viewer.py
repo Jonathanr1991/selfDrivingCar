1c5d2ffb3cfaeb0da1a1239423491803abb1ae9d
jrodriquez <jrodri30@students.towson.edu>
2019-03-03 11:26:49 -0500

    streamer and viewer update

--- Viewer.py
+++ Viewer.py
@@ -20,19 +20,25 @@ conn,addr=s.accept()
 
 ### new
 data = ""
-payload_size = struct.calcsize("H")
+payload_size = struct.calcsize("L")
 while True:
     while len(data) < payload_size:
         data += conn.recv(4096)
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
-    msg_size = struct.unpack("H", packed_msg_size)[0]
+    msg_size = struct.unpack("L", packed_msg_size)[0]
     while len(data) < msg_size:
         data += conn.recv(4096)
     frame_data = data[:msg_size]
     data = data[msg_size:]
     ###
 
-frame=pickle.loads(frame_data)
-print frame
-cv2.imshow('frame',frame)
+    frame=pickle.loads(frame_data)
+    print frame
+    cv2.imshow('frame',frame)
+    
+    key = cv2.waitKey(10)
+    if (key == 27) or (key == 113):
+        break
+
+cv2.destroyAllWindows()
