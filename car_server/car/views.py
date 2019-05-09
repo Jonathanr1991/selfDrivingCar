fbaf97a2260dbc63fdc7e7177fcf625edf30ea46
jrodriquez <jrodri30@students.towson.edu>
2019-05-09 02:18:08 -0400

    server setup

--- car_server/car/views.py
+++ car_server/car/views.py
@@ -8,31 +8,31 @@ from  django.http import HttpResponse
 class Controller():
     #car = CorvetteController()
 
-    def home_view(request, *args, **kwargs):
+    def home_view(self,request, *args, **kwargs):
         return render(request,'dashboard.html', {})
 
-    def go_foward(request):
+    def go_foward(self,request):
         direction = request.POST.get('foward')
         print(direction)
        # if direction=='foward':
          #   car.foward()
         return render(request, 'dashboard.html',{})
 
-    def go_back(request):
+    def go_back(self, request):
         direction = request.POST.get('Back')
         print(direction)
        # if direction=="Back":
         #    car.reverse()
         return render(request, 'dashboard.html',{})
         
-    def go_right(request):
+    def go_right(self, request):
         direction = request.POST.get('Right')
         print(direction)
       #  if direction=="Right":
        #     car.right()
         return render(request, 'dashboard.html',{})
     
-    def go_left(request):
+    def go_left(self, request):
         direction = request.POST.get('Left')
         print(direction)
         #if direction=="Left":
